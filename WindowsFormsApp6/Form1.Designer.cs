db08569aa7515f5ea8d0fc8cbc349e6311646429
MustafazadeRamazan <workman24680@gmail.com>
2022-02-18 18:07:10 +0400

    Tapsiriq

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.font_box = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.size_box = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.color_box = new System.Windows.Forms.ComboBox();
+            this.loadtext_box = new System.Windows.Forms.TextBox();
+            this.savetext_box = new System.Windows.Forms.TextBox();
+            this.load_btn = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(83, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Font";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(953, 407);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // save_btn
+            // 
+            this.save_btn.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.save_btn.Location = new System.Drawing.Point(917, 47);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(71, 22);
+            this.save_btn.TabIndex = 1;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.savebtn_click);
+            // 
+            // font_box
+            // 
+            this.font_box.FormattingEnabled = true;
+            this.font_box.Location = new System.Drawing.Point(51, 46);
+            this.font_box.Name = "font_box";
+            this.font_box.Size = new System.Drawing.Size(121, 21);
+            this.font_box.TabIndex = 3;
+            this.font_box.Text = "sfaf";
+            this.font_box.SelectedIndexChanged += new System.EventHandler(this.font_box_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(229, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Size";
+            // 
+            // size_box
+            // 
+            this.size_box.FormattingEnabled = true;
+            this.size_box.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "36",
+            "48",
+            "72"});
+            this.size_box.Location = new System.Drawing.Point(215, 46);
+            this.size_box.Name = "size_box";
+            this.size_box.Size = new System.Drawing.Size(81, 21);
+            this.size_box.TabIndex = 3;
+            this.size_box.Text = "8";
+            this.size_box.SelectedIndexChanged += new System.EventHandler(this.font_box_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(335, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "FontStyle";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(470, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "FontStyle";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(470, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Alignment";
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(475, 43);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(27, 26);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "L";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(508, 43);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(27, 26);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "C";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(541, 43);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(27, 26);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "R";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(620, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Color";
+            // 
+            // color_box
+            // 
+            this.color_box.FormattingEnabled = true;
+            this.color_box.Location = new System.Drawing.Point(606, 46);
+            this.color_box.Name = "color_box";
+            this.color_box.Size = new System.Drawing.Size(94, 21);
+            this.color_box.TabIndex = 3;
+            this.color_box.SelectedIndexChanged += new System.EventHandler(this.color_box_SelectedIndexChanged);
+            // 
+            // loadtext_box
+            // 
+            this.loadtext_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadtext_box.Location = new System.Drawing.Point(728, 19);
+            this.loadtext_box.Name = "loadtext_box";
+            this.loadtext_box.Size = new System.Drawing.Size(174, 22);
+            this.loadtext_box.TabIndex = 5;
+            // 
+            // savetext_box
+            // 
+            this.savetext_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.savetext_box.Location = new System.Drawing.Point(728, 47);
+            this.savetext_box.Name = "savetext_box";
+            this.savetext_box.Size = new System.Drawing.Size(174, 22);
+            this.savetext_box.TabIndex = 5;
+            // 
+            // load_btn
+            // 
+            this.load_btn.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load_btn.Location = new System.Drawing.Point(917, 19);
+            this.load_btn.Name = "load_btn";
+            this.load_btn.Size = new System.Drawing.Size(71, 22);
+            this.load_btn.TabIndex = 1;
+            this.load_btn.Text = "Load";
+            this.load_btn.UseVisualStyleBackColor = true;
+            this.load_btn.Click += new System.EventHandler(this.loadbtn_click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(340, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(25, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "B";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(371, 47);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(25, 24);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "U";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox3.Location = new System.Drawing.Point(402, 47);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(25, 24);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "I";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.savetext_box);
+            this.Controls.Add(this.loadtext_box);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.color_box);
+            this.Controls.Add(this.size_box);
+            this.Controls.Add(this.font_box);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.load_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button load_btn;
+        public System.Windows.Forms.ComboBox font_box;
+        public System.Windows.Forms.ComboBox size_box;
+        public System.Windows.Forms.ComboBox color_box;
+        public System.Windows.Forms.TextBox loadtext_box;
+        public System.Windows.Forms.TextBox savetext_box;
+        public System.Windows.Forms.Button button9;
+        public System.Windows.Forms.Button button10;
+        public System.Windows.Forms.Button button11;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            font_box.Items.AddRange((from i in FontFamily.Families select i.Name).ToArray());
+            color_box.Items.Clear();
+            string[] colores = Enum.GetNames(typeof(System.Drawing.KnownColor));
+            color_box.Items.AddRange(colores);
+        }
+
+        private void loadbtn_click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "All files|*.*|Text files|*.txt";
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using(StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                {
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void savebtn_click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.Write(richTextBox1.Text);
+                }
+            }
+        }
+
+        private void font_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(font_box.SelectedItem.ToString(), float.Parse(size_box.SelectedItem.ToString()));
+        }
+
+        private void color_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionColor = Color.FromName(color_box.Text);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender is CheckBox cb)
+            {
+                if (cb.Checked)
+                {
+                    var fontStyle = richTextBox1.SelectionFont.Style;
+                    if (cb.Text == "B")
+                        fontStyle |= FontStyle.Bold;
+                    if (cb.Text == "U")
+                        fontStyle |= FontStyle.Underline;
+                    if (cb.Text == "I")
+                        fontStyle |= FontStyle.Italic;
+
+                    richTextBox1.SelectionFont = new Font(font_box.SelectedItem.ToString(), float.Parse(size_box.SelectedItem.ToString()), fontStyle);
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+        }
+    }
+}
